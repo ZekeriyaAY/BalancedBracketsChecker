376d8c0bbde651eb4eaed186d2a229efc875b065
Zekeriya AY <zekeriya@zekeriyaay.com>
2022-01-22 12:28:29 +0300

    Updated print lines

--- main.cpp
+++ main.cpp
@@ -10,11 +10,12 @@
 #include <stack>
 
 using namespace std;
-
 int main() {
     ifstream test_file;
     stack<char> stack;
     char data;
+    int count = 0;
+    int cmdLineIndex = -1; 
     bool is_comment = false;
     bool unmatched = false;
 
@@ -22,12 +23,16 @@ int main() {
     stack.push('X');
     if (test_file.is_open()) {
         while (test_file >> data) {
+            count++;
+            cout << data << " ";
+
             /* [CHECK] In comment block? */
             if (is_comment && data == '*' && stack.top() == '*') {
                stack.pop();
                continue;
             } else if (is_comment && data == '/' && stack.top() == '/') {
                 is_comment = false;
+                cmdLineIndex = -1;
                 stack.pop();
                 continue;
             } else if (is_comment) {
@@ -40,12 +45,14 @@ int main() {
                 continue;
             } else if (data == '*' && stack.top() == '/') {
                 is_comment = true;
+                cmdLineIndex = count;
                 stack.push(data);
                 continue;
             } 
             /* False Positive Comment Block */
             else if(stack.top() == '/' && data != '*'){
                 is_comment = false;
+                cmdLineIndex = -1;
                 stack.pop();
                 continue;
             }
@@ -72,15 +79,15 @@ int main() {
 
             /* [CHECK] Non-closing Brackets  */
             if (data == ')' && stack.top() != '(') {
-                cout << "[!]Unmatched ')' Exiting..." << endl;
+                cout << endl << "[!]" << count << ". karakter olan ')' ile eşleşme olmadı..." << endl;
                 unmatched = true;
                 break;
             } else if (data == ']' && stack.top() != '[') {
-                cout << "[!]Unmatched ']' Exiting..." << endl;
+                cout << endl << "[!]" << count << ". karakter olan ']' ile eşleşme olmadı..." << endl;
                 unmatched = true;
                 break;
             } else if (data == '}' && stack.top() != '{') {
-                cout << "[!]Unmatched '}' Exiting..." << endl;
+                cout << endl << "[!]" << count << ". karakter olan '}' ile eşleşme olmadı..." << endl;
                 unmatched = true;
                 break;
             }
@@ -89,14 +96,14 @@ int main() {
 
     /* [CHECK] Is the Stack empty? */
     if (!stack.empty() && stack.top() != 'X') {
-        cout << "[?]Stack is not empty" << endl << "[-]Stack-> ";
+        cout << endl << "[?]Stack boş değil. Eşleşmeyen karakterler var." << endl << "[-]Stack-> ";
         while (!stack.empty()) {
             if (stack.top() != 'X') {
                 if (stack.top() == '*') {
                     is_comment = true;
                 } else if (is_comment == true && stack.top() == '/') {
                     is_comment = false;
-                    cout << "[!]Unmatched '*/' in stack" << endl;
+                    cout << "[!]" << cmdLineIndex << ". karakter olan '/*' yorum satırı kapatılmadı..." << endl;
                 }
                 cout << stack.top();
             }
@@ -104,7 +111,7 @@ int main() {
         }
         cout << endl;
     } else if (!unmatched) {
-        cout << "[+]Compilation Successful!" << endl;
+        cout << endl << "[+]Derleme Basarili" << endl;
     } 
     
     test_file.close();
